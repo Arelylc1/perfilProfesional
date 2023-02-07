652f181f173907695d7713d1c42085dc02ef6f02
Arely Lopez <lcervantesarely@gmail.com>
2023-02-07 00:54:54 -0600

    Perfil profesional modificado

---
+++ JS/main.js
@@ -0,0 +1,18 @@
+// let btn_nombre = document.getElementById("btn_name");
+// let input_nombre = document.getElementById("input_name")
+
+
+// btn_nombre.addEventListener("click", myFunction);
+
+// function myFunction() {
+//   btn_nombre.innerHTML = 
+//   '<br>' +
+//   '<br>' +
+//   '<div class="card">' +
+//   '<div class="alert alert-success" role="alert">' +
+//   '<h4 class="alert-heading">Hola' + input_nombre + '</h4>'+
+//   '<p>Gracias por visitar mi perfil profesional</p>'
+//   '</div>'
+//   '</div>';
+
+// }
